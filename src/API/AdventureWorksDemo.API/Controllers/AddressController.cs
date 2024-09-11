36f77c4c7789f26bd767178242c871bd90b37328
CodeTile <43536260+CodeTile@users.noreply.github.com>
2024-09-11 13:45:00 +0100

    Comment out AddressController

--- src/API/AdventureWorksDemo.API/Controllers/AddressController.cs
+++ src/API/AdventureWorksDemo.API/Controllers/AddressController.cs
@@ -1,12 +1,12 @@
-using AdventureWorksDemo.Data.Models;
-using AdventureWorksDemo.Data.Services;
-using Microsoft.AspNetCore.Mvc;
+//using AdventureWorksDemo.Data.Models;
+//using AdventureWorksDemo.Data.Services;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace AdventureWorksDemo.API.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class AddressController(ILogger<AddressController> logger, IAddressService service) : GenericControllerBase<AddressModel>(logger, service)
-    {
-    }
-}
+//namespace AdventureWorksDemo.API.Controllers
+//{
+//    [ApiController]
+//    [Route("api/[controller]")]
+//    public class AddressController(ILogger<AddressController> logger, IAddressService service) : GenericControllerBase<AddressModel>(logger, service)
+//    {
+//    }
+//}
